b1b833f84f717c93026158b58f4194b693d28d82
Thiago Puntar <thiagopuntar@gmail.com>
2024-03-02 12:31:37 -0300

    encontro 1

---
+++ app.js
@@ -0,0 +1,12 @@
+// common JS
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/products", (req, res) => {
+  res.send("Hello World!2");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
